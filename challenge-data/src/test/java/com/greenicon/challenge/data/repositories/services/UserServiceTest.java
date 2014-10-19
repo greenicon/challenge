94af28dccaea72e0c9c993f9d6275c240098746b
pranav-gopi <pranavgopi@ootlook.com>
2014-10-19 13:53:00 +0530

    Added User Junit Test cases

---
+++ challenge-data/src/test/java/com/greenicon/challenge/data/repositories/services/UserServiceTest.java
@@ -0,0 +1,101 @@
+/**
+ * 
+ */
+package com.greenicon.challenge.data.repositories.services;
+
+import static org.junit.Assert.fail;
+
+import java.util.Collection;
+import java.util.Date;
+
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Ignore;
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+
+import com.greenicon.challenge.data.models.User;
+import com.greenicon.challenge.data.repositories.services.provider.RepositoryServiceProvider;
+import com.greenicon.challenge.data.util.TestUtils;
+
+/**
+ * @author pranav.gopi
+ * @createdDate Oct 19, 2014
+ */
+public class UserServiceTest {
+
+	static RepositoryServiceProvider rsp;
+	static UserService userService;
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+		ApplicationContext applicationContext = TestUtils.getApplicationContext();
+		rsp = applicationContext.getBean(RepositoryServiceProvider.class);
+		userService = rsp.getUserService();
+	}
+
+	/**
+	 * Test method for {@link com.greenicon.challenge.data.repositories.services.UserService#create(com.greenicon.challenge.data.models.User)}.
+	 */
+	@Test
+	public void testCreate() {
+		User user = new User();
+		user.setFirstName("First");
+		user.setLastName("sfc");
+		user.setEmail("a");
+		user.setMobilenumber("22");
+		user.setDob(new Date());
+		user.setCoverImageid("1");
+		user.setProfileImageid("2");
+		
+		userService.create(user);
+	}
+
+	/**
+	 * Test method for {@link com.greenicon.challenge.data.repositories.services.UserService#delete(java.lang.String)}.
+	 */
+	@Test
+	public void testDelete() {
+		Collection<User> users = userService.findAll();
+		for(User user:users){
+		 userService.delete(user.getId());
+		 break;
+		}
+	}
+
+	/**
+	 * Test method for {@link com.greenicon.challenge.data.repositories.services.UserService#update(com.greenicon.challenge.data.models.User)}.
+	 */
+	@Ignore
+	@Test
+	public void testUpdate() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link com.greenicon.challenge.data.repositories.services.UserService#find(java.lang.String)}.
+	 */
+	@Ignore
+	@Test
+	public void testFind() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link com.greenicon.challenge.data.repositories.services.UserService#findAll()}.
+	 */
+	@Test
+	public void testFindAll() {
+		System.out.println(userService.findAll());
+	}
+
+}
---
+++ challenge-data/src/test/java/com/greenicon/challenge/data/util/TestUtils.java
@@ -0,0 +1,11 @@
+package com.greenicon.challenge.data.util;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class TestUtils {
+
+	public static ApplicationContext getApplicationContext(){
+		return new ClassPathXmlApplicationContext("classpath:SpringContext.xml");
+	}
+}

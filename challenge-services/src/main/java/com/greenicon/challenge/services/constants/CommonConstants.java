d40ef3e17ded7b8afcdaa5d0e6d64fdce4e3a2f4
pranav-gopi <pranavgopi@ootlook.com>
2014-10-19 15:12:55 +0530

    CommonConstants and CommonUtils

---
+++ challenge-services/src/main/java/com/greenicon/challenge/services/constants/CommonConstants.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package com.greenicon.challenge.services.constants;
+
+
+/**
+ * @createdDate Oct 19, 2014
+ */
+public interface CommonConstants {
+
+	/**
+	 * Common Utils Constants
+	 */
+	final String TOMCAT_HOME = System.getProperty("catalina.home");
+	final String DEFAULT_PROPERTY_FILE = "config";
+	final String CHALLENGE_CONF_PATH = TOMCAT_HOME+"/conf/challenge/services";
+	
+}

865f426e462908ed67818de482ab3c8ddbf3637d
likeshvp <likeshvp@gmail.com>
2014-11-01 12:10:34 +0530

    challenge List

--- challenge-services/src/main/java/com/greenicon/challenge/services/challengelist/ChallengeListServices.java
+++ challenge-services/src/main/java/com/greenicon/challenge/services/challengelist/ChallengeListServices.java
@@ -2,17 +2,22 @@ package com.greenicon.challenge.services.challengelist;
 
 import java.util.List;
 
+import com.greenicon.challenge.data.models.Challenge;
 import com.greenicon.challenge.data.models.ChallengeList;
+import com.greenicon.challenge.services.common.exceptions.ChallengeException;
 
 public interface ChallengeListServices {
 	
-	void CreateChallengeList(ChallengeList challengeList) throws Exception;
-	void AddChallengeToChallengeList(String challengeId,String challengeListId)throws Exception;
-	void RomoveChallengeFromChallengeList(String challengeId,String challengeListId)throws Exception;
-	void DeleteChallengeList(String challengeListId)throws Exception;
-	ChallengeList getChallengeListById(String id)throws Exception;
-	List<ChallengeList> getAllChallengeListOfAUser(String UserId) throws Exception;
-	List<ChallengeList> getAllChallengeListByName(String name) throws Exception;
-	List<ChallengeList> getAllChallengeListOfAUserByName(String UserId,String name) throws Exception;	
+	void CreateChallengeList(ChallengeList challengeList) throws ChallengeException;
+	void AddChallengeIdToChallengeList(String challengeId,String challengeListId)throws ChallengeException;
+	void AddListOfChallengeIdsToChallengeList(List<String> challengeIdList,String challengeListId)throws ChallengeException;
+	void AddChallengeToChallengeList(Challenge challenge,String challengeListId)throws ChallengeException;
+	void AddListOfChallengesToChallengeList(List<Challenge> challengeList,String challengeListId)throws ChallengeException;
+	void RomoveChallengeFromChallengeList(String challengeId,String challengeListId)throws ChallengeException;
+	void DeleteChallengeList(String challengeListId)throws ChallengeException;
+	ChallengeList getChallengeListById(String id)throws ChallengeException;
+	List<ChallengeList> getAllChallengeListOfAUser(String UserId) throws ChallengeException;
+	List<ChallengeList> getAllChallengeListByName(String name) throws ChallengeException;
+	List<ChallengeList> getAllChallengeListOfAUserByName(String UserId,String name) throws ChallengeException;	
 	
 }
